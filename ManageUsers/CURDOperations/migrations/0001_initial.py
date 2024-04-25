f22285fc9c2f4f66c75f4b61aa787b4736a284fe
Nanthagopal Shanmugam <Nanthagopal.s@mantralabs.tech>
2024-04-25 15:38:31 +0530

    Ff

--- ManageUsers/CURDOperations/migrations/0001_initial.py
+++ ManageUsers/CURDOperations/migrations/0001_initial.py
@@ -16,12 +16,12 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=50)),
-                ('dob', models.DateField()),
+                ('dob', models.DateField()), 
                 ('email', models.EmailField(max_length=254)),
                 ('mobile_number', models.BigIntegerField()),
             ],
             options={
                 'db_table': 'UserDetails',
             },
-        ),
+        ), #hhhh
     ]
